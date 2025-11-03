d68df6d3900615e29ffa3625a067112fdd47c0e9
jeromo47 <jmolinacaparros@gmail.com>
2025-11-03 16:11:24 +0100

    Create id.ts

---
+++ src/app/id.ts
@@ -0,0 +1,10 @@
+export function rid(): string {
+  try {
+    if ("randomUUID" in crypto) return (crypto as any).randomUUID();
+  } catch {}
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {
+    const r = (Math.random() * 16) | 0,
+      v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
